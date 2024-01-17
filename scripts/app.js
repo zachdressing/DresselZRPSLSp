64ccd0d2b374aa236af6c52ecd95d6f078c120ee
zachdressing <zed_dressel@hotmail.com>
2024-01-17 13:29:56 -0800

    Major Changes, added Instructions for game

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,17 @@
 const apiURL = "https://rpslsapi.azurewebsites.net/RPSLS";
+let instructButt = document.getElementById('instructionsButton');
+let instructImg = document.getElementById('instructionImg');
 
-async function APICall(){
+const bestofArray = [1, 3, 7];
+
+async function APICall() {
     const promise = await fetch(apiURL);
     const data = await promise.text();
     console.log(data);
 }
 
+instructButt.addEventListener('click', e => {
+    console.log(instructImg.src)
+    if (instructImg.src.includes("InstructionText")) { instructImg.src = "./assets/images/Instructions.png" }
+    else { instructImg.src = "./assets/images/InstructionText.png" }
+});
